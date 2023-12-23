f1e31f2a4558f41fe050c365fbd4366ef98bf504
rider <bishop.r@northeastern.edu>
2023-12-23 12:57:26 -0500

    fixed files

--- src/Components/NewsroomComponents/NewsLetterItems/newsLetterConstants.js
+++ src/Components/NewsroomComponents/NewsLetterItems/newsLetterConstants.js
@@ -1,9 +1,7 @@
 import venturesImage from "../../../img/homeVenturesProgram.svg";
 import consultingImage from "../../../img/homeConsultingProgram.svg";
 import eventsImage from "../../../img/homeEventsProgram.svg";
-import author1 from "../../../img/Aidan O'Brien.png";
-import author2 from "../../../img/Leo Cochrane.png";
-import author3 from "../../../img/Rider Bishop.jpg";
+import author3 from "../../../img/rider_bishop.jpg";
 
 export const newsletter = [
   {
@@ -11,7 +9,7 @@ export const newsletter = [
     date: "January 1, 2024",
     description:
       "Lorem ipsum dolor sit amet consectetur. Adipiscing quis eu mi dolor blandit. Lorem ipsum dolor sit amet consectetur. Adipiscing quis eu mi dolor blandit.",
-    authorHeadshot: author1,
+    authorHeadshot: author3,
     author: "Aidan O'Brien",
     img: venturesImage,
     readMoreLink: "#",
@@ -21,7 +19,7 @@ export const newsletter = [
     date: "January 1, 2024",
     description:
       "Lorem ipsum dolor sit amet consectetur. Adipiscing quis eu mi dolor blandit. Lorem ipsum dolor sit amet consectetur. Adipiscing quis eu mi dolor blandit.",
-    authorHeadshot: author2,
+    authorHeadshot: author3,
     author: "Leo Cochrane",
     img: consultingImage,
     navLink: "#",
