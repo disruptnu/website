7dfd1d19c17fb79eaf52867b217ec7c9155b04d7
rider <bishop.r@northeastern.edu>
2023-12-23 13:37:44 -0500

    another

--- src/Components/NewsroomComponents/FeaturedNews.js
+++ src/Components/NewsroomComponents/FeaturedNews.js
@@ -1,8 +1,33 @@
 import { CARD } from "./newsroomConstants";
-import FeaturedCard from "./FeaturedNews/FeaturedCard";
 import React, { useRef } from "react";
 import leftScroll from "../../img/LeftGreenArrow.svg";
 import rightScroll from "../../img/RightGreenArrow.svg";
+
+function FeaturedCard(props) {
+  return (
+    <div className="px-10 flex flex-col items-center min-w-max min-h-max">
+      <img
+        className="rounded-sm pb-2 w-130"
+        src={props.image}
+        alt={props.alt}
+      />
+      <div className="flex justify-between w-130 mx-5">
+        <h3
+          className="text-white font-bold"
+          style={{
+            fontFamily: "Faucet",
+            fontFeatureSettings: "'ss03' on, 'ss02' on, 'ss01' on",
+          }}
+        >
+          {props.header}
+        </h3>
+        <p className="text-white text-sm font-bold border border-solid border-white rounded-md py-1 px-2 text-center">
+          Tag
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export default function FeaturedNews() {
   const containerRef = useRef(null);
